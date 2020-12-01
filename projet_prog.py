d48a6bdc49afe46dd2030b1ef476d2baeff40cb9
Anouk-H <72785840+Anouk-H@users.noreply.github.com>
2020-12-01 20:04:13 +0100

    Update projet_prog.py

--- projet_prog.py
+++ projet_prog.py
@@ -9,6 +9,7 @@ url = "https://raw.githubusercontent.com/margueritepap/projet_eivp_info/main/don
 download = requests.get(url).content #le télécharge
 
 df = pd.read_csv(io.StringIO(download.decode('utf-8'))) #lis le contenu du fichier et le change en données exploitables par pandas (dataframe=df)
+df['sent_at']=pd.to_datetime(df.sent_at)                 #pour que les dates soient considérées comme du temps
 
 
 # print (df) #affiche toutes les données du fichier
@@ -19,30 +20,6 @@ tete=df.columns.values.tolist()
 print (donnees_lignes)
 print(tete)
 
-#On doit maintenant remplacer les ; par des ,
-#obsolète
-def virgule(D): #remplace les ; par des ,
-    l=len(D)
-    A=[]
-    n=0
-    for k in range (l):
-        i=D[k][0]
-        n=i.replace(';',',')
-        A+=[[n]]
-    return A
-
-def preleve_colonne(D,n): #prélève la colonne n du tableau D et la renvoie sous forme d'une liste
-    l=len(D)
-    a=D[0][0] #on sépare les entètes du reste du tableau
-    b=[a.split(';')]
-    c=b[0][n]
-    S=[c]
-    for k in range (1,l):
-        a=D[k][0]
-        b=[a.split(';')] #change les ; en ,
-        c=float(b[0][n]) #change le str en chiffre
-        S+=[c]
-    return S
 
 
 ## Evolution d’une variable en fonction du temps
@@ -50,21 +27,20 @@ def preleve_colonne(D,n): #prélève la colonne n du tableau D et la renvoie sou
 ## Calcul des valeurs statistiques
 from math import * #pour le calcul de l'écart type
 
-def min(L):
-    m=L[0]
-    for k in range (len(L)):
-        t=L[k]
-        if t<m:
-            m=t
-    return m
 
-def max(L):
-    m=L[0]
-    for k in range (len(L)):
-        t=L[k]
-        if t>m:
-            m=t
-    return m
+def min_max(doc,nom):                         #nom=le nom de la colonne dont veut le min/max
+    min=doc[nom][0]
+    max=doc[nom][0]
+    i=0                                       #indice du min
+    I=0                                       #indice du max
+    for k in range(doc.shape[0]):
+        if doc[nom][k]<min:
+            min=doc[nom][k]
+            i=k
+        elif doc[nom][k]>max:
+            max=doc[nom][k]
+            I=k
+    return min,i,max,I
 
 def moyenne(L):
     l=len(L)
@@ -73,17 +49,30 @@ def moyenne(L):
         m+=L[k]
     return m/l
 
+def tri_rapide(L):
+    if L==[]:
+        return L
+    L_g=[]
+    L_d=[]
+    for i in range(1,len(L)):
+        if L[i]<=L[0]:
+            L_g.append(L[i])
+        else:
+            L_d.append(L[i])
+    return tri_rapide(L_g)+[L[0]]+tri_rapide(L_d)
+
 def mediane(L):
+    Li=tri_rapide(L)
     l=len(L)
     m=int(l%2) #parité du nb d'éléments de la liste
     if l!=0:
         if m==0: #si nb pair d'éléments
             i=int(l/2)
-            mo=(L[i-1]+L[i+1])/2
+            mo=(Li[i-1]+Li[i+1])/2
             return int(mo)
         else: #si nb impaire d'éléments
             i=int((l-1)/2)
-            return L[i]
+            return Li[i]
     return "La liste est vide"
 
 def variance(L):
@@ -141,17 +130,6 @@ def indice_correlation_Barvais_Pearson(A,B): #pour les valeurs régulières, peu
 #Calcul de l'indice de corrélation de Spearman:
 
 #pour des valeurs qui présentent des irrégularités, permet de mettre en évidence une relation non lénaire
-def tri_rapide(L):
-    if L==[]:
-        return L
-    L_g=[]
-    L_d=[]
-    for i in range(1,len(L)):
-        if L[i]<=L[0]:
-            L_g.append(L[i])
-        else:
-            L_d.append(L[i])
-    return tri_rapide(L_g)+[L[0]]+tri_rapide(L_d)
 
 def rang(a,L): #donne le rang de la première apparaition l'élément a dans la liste L
     T=tri_rapide(L)
@@ -179,6 +157,61 @@ def indice_correlation_Spearman(A,B):
     return cov/(ea*eb)
 
 
+def fusion(l1,l2):
+    if l1==[]:
+        return l2
+    elif l2==[]:
+        return l1
+    elif l1[0]<l2[0]:
+        return [l1[0]]+fusion(l1[1:],l2)
+    else:
+        return [l2[0]]+fusion(l1,l2[1:])
+
+
+def tri_fusion(liste):
+    if len(liste)<=1:
+        return liste
+    else:
+        n=len(liste)
+        L1=liste[:n//2]
+        L2=liste[n//2:]
+        return fusion(tri_fusion(L1),tri_fusion(L2))
+    
+def locali_dates(doc,heure):                     #renvoie la ligne de la date "la plus proche" dans le doc de celle qu'on a rentré dans la fonction, on a besoin de la même écriture de l'heure
+    tri_rapide(doc['sent_at'])                   
+    a=0
+    b=doc.shape[0]                               #car c'est le nombre de lignes
+    c=doc.shape[0]//2
+    d=timedelta(minutes=1)                       #si on sait qu'on rentre une heure du tableau on peut mettre un temps plus court, c'était pour être sûre que la boucle s'arrêterait toujours
+    if heure<=doc['sent_at'][0]:
+        return 0
+    elif heure>=doc['sent_at'][doc.shape[0]-1]:
+        return doc.shape[0]-1
+    else:
+        while abs(heure-doc['sent_at'][c])>d:    #problème, en fonction de d on aura pas forcément la date la plus proche mais c'est pour être sûr que la boucle s'arrêtera pour toutes/un max de dates
+            if heure<=doc['sent_at'][c]:
+                b=c
+                c=c//2
+            else:
+                a=c
+                c=(a+b)//2
+        return c
+
+
+    
+ def Graphe(doc, nom, start=fichi['sent_at'][0], end=fichi['sent_at'][fichi.shape[0]-1]):     #nom correspond au nom de la colonne (doit être entre guillemets), ne marque que pour le fichier fichi
+    Y=doc['sent_at']                                                                          #ça marche parce qu'on connait le nom de la colonne avec les dates
+    X=[doc[nom][k] for k in range(doc.shape[0])]                                              #dans le range c'est le nombre de lignes
+    med=mediane(doc,nom)[0]
+    moy=moyenne(doc,nom)
+    plt.close()
+    plt.plot(X,Y)
+    plt.plot(min_max(doc,nom)[0],min_max(doc,nom)[1], color='black', marker=(5,1))
+    plt.plot(min_max(doc,nom)[2],min_max(doc,nom)[3], color='black', marker=(5,1))
+    plt.plot([med,med],[0,doc['sent_at'][doc.shape[0]-1]], color='green')                     #trace une ligne horizontale pour la médiane
+    plt.plot([moy,moy],[0,doc['sent_at'][doc.shape[0]-1]], color='magenta')                   #trace une ligne horizontale pour la moyenne
+    pyplot.title('Evolution de ', nom, 'en octobre 2019')
+    plt.show()
 
 
 
